2fbffbeee86d9d99a2bf24785ccbb8a4b2a8a00d
ilizarraga278 <ilizarraga612@gmail.com>
2024-10-01 12:21:54 -0700

    library and my library classes

---
+++ MyLibrary.java
@@ -0,0 +1,6 @@
+// should contain main method that runs text based user interface
+// where user can enter commands to get or add info to library
+// MyLibrary is user interface
+public class MyLibrary {
+
+}
