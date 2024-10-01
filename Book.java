72f3a013166437e6b2ec4e9bde84e0360f4928ad
ilizarraga278 <ilizarraga612@gmail.com>
2024-10-01 12:18:21 -0700

    Add files via upload
    book class recommit

---
+++ Book.java
@@ -0,0 +1,45 @@
+// will model a book, and produce book objects that
+// can be stored int the Library class
+public class Book {
+	//--more instance variables here--
+	private String title;
+	private String author;
+	private int rating; 
+	private boolean isRead;// rating should be 1-5 can be changed, maybe
+	                    // change visibility later
+		
+	public Book() {
+		this.title = title;
+		this.author = author;
+		this.rating = rating;
+		this.isRead = false; // default should be false?
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+	
+	// add rating to a book if rating attribute is null
+	public void setRating(int rating) {
+		
+	}
+	
+	// return true if book is read, false if not
+	public boolean isRead() {
+		
+	}
+	
+	// change read status of book
+	public void read() {
+		
+		
+	}
+}
