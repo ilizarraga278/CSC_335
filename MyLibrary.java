01f69e01c5330c8c582812ef45e157e32a981f35
ilizarraga278 <ilizarraga612@gmail.com>
2024-10-04 15:16:38 -0700

    my library

--- MyLibrary.java
+++ MyLibrary.java
@@ -1,6 +1,48 @@
 // should contain main method that runs text based user interface
 // where user can enter commands to get or add info to library
 // MyLibrary is user interface
+
+import java.util.Scanner;
+
 public class MyLibrary {
+	public static void main(String args[]) {
+		Library myLibrary = new Library();
+		Scanner keyboard = new Scanner(System.in);
+		boolean ctrue = true;
+		
+		while(true) {
+			System.out.println("Enter a command: ");
+			String command = keyboard.nextLine();
+			command(command.toLowerCase(),myLibrary);
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
+
+	private static void command(String command, Library myLibrary) {
+		if (command.equals("search")) {
+			System.out.println("Enter a search method: ");
+			Scanner keyboard = new Scanner(System.in);
+			String searchMethod = keyboard.nextLine();
+			System.out.println("Enter the search id/info: ");
+			String searchId = keyboard.nextLine();// might have to add toLowerCase() later
+			myLibrary.search(searchMethod, searchId);
+			
+		}
+		if (command.equals("addBook")) {
+			System.out.println("Enter the title and author: "){
+				
+			}
+		}
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
